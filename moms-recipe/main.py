c185e9d27b882f4f7003be6f31fd5d6fbf48bbf3
Nigel Harvey <harveyni@bc.edu>
2017-07-24 15:34:03 -0400

    Server Set up 3

--- moms-recipe/main.py
+++ moms-recipe/main.py
@@ -29,8 +29,10 @@ jinja_environment = jinja2.Environment(loader =
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('templates/main.html')
-
-        self.response.write('Hello world!')
+        temp = {
+        
+        }
+        self.response.write(template.render(temp))
 
 class RecipeHandler(webapp2.RequestHandler):
     def get(self):
